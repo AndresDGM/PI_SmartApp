b28b79e3a2f033f6643c31e388856fa680e4bd33
pokemaster345x <135090785+pokemaster345x@users.noreply.github.com>
2023-09-26 05:56:13 -0500

    bobaita namas
    si

--- Proyecto/src/operaMatrices.java
+++ Proyecto/src/operaMatrices.java
@@ -1,12 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author ekava
- */
-public class operaMatrices {
+import java.util.Scanner;
+public class OperaMatrices {
+    static Scanner sc = new Scanner(System.in);
+    double matriz1[][],matriz2[][];
+    int f1,f2,c1,c2;// se le asignara los valores 
+    public void obtenertamaños(){
+        //toda esta monda sera cambiada por el jframe hpta vida la mia 
+        System.out.println("a continuacion se obtendra el tamaño del primer vector");
+        System.out.println("ingrese el numero de filas");
+        f1=sc.nextInt();
+        System.out.println("ingrese el numero de columnas");
+        c1=sc.nextInt();
+        matriz1= new double[f1][c1];
+        System.out.println("datos de la segunda matriz:");
+        System.out.println("ingrese el numero de filas");
+        f2=sc.nextInt();
+        System.out.println("ingrese el numero de columna");
+        c2=sc.nextInt();
+        matriz2= new double[f1][c1];
+    }
+    public void llenar(){
+        
+    }
+    public void compAndMulti(){
+        double matrizR[][]= new double[f1][c2];
+        if (c1==f2) {
+            
+        } else {
+            System.out.println("bobo hpta no son compatibles para multiplicar");
+        }
+    }
     
 }
